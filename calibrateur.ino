c5160dfc57887df3de2c0b6acc130638734ae81a
MajorLee <joel@vorobotics.com>
2015-12-13 00:30:46 +0100

    ajout des fichiers de classe dans le projet

--- calibrateur.ino
+++ calibrateur.ino
@@ -1,3 +1,20 @@
+/**
+#*******************************************************************************
+# Projet : VOR-005
+# Sous projet : calibrateur de servo moteurs
+# Auteurs: Gille De Bussy
+#
+# class servo:
+# permet de piloter les servo qui sont utilises sur le proet
+# servo en test mais aussi servo de la potence
+# utilise la librairie servo de chez Adafruit pour la carte
+Adafruit 16-Channel 12-bit PWM/Servo Driver - I2C interface - PCA9685
+PRODUCT ID: 815
+#*******************************************************************************
+ J.Soranzo
+	26/11/2015 gestion sous git + github
+	https://github.com/gilles293/calibrateurServo.git
+*/
 #include <Arduino.h>
 
 /*
@@ -7,7 +24,7 @@
 void setup()
 {
 	Serial.begin(9600);
-	Serial.println(F("Hello world!"));
+	Serial.println(F("Test fourche optique"));
 	// initialize the digital pin as an output.
 	// Pin 13 has an LED connected on most Arduino boards:
 	pinMode(13, OUTPUT);
