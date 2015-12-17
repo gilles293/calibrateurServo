ea3a72c2104abec433f7947bd527de121a5f51b9
MajorLee <joel@vorobotics.com>
2015-12-17 22:53:39 +0100

    sweep servo implémente avec la librairie servo standard

--- calibrateur.ino
+++ calibrateur.ino
@@ -2,36 +2,49 @@
 #*******************************************************************************
 # Projet : VOR-005
 # Sous projet : calibrateur de servo moteurs
-# Auteurs: Gille De Bussy
+# Auteurs: J.Soranzo
 #
-# class servo:
+# Programme principal: test de la fourche optique
 # permet de piloter les servo qui sont utilises sur le proet
 # servo en test mais aussi servo de la potence
 # utilise la librairie servo de chez Adafruit pour la carte
-Adafruit 16-Channel 12-bit PWM/Servo Driver - I2C interface - PCA9685
-PRODUCT ID: 815
 #*******************************************************************************
  J.Soranzo
-	26/11/2015 gestion sous git + github
+	17/12/2015 gestion sous git + github
 	https://github.com/gilles293/calibrateurServo.git
 */
 #include <Arduino.h>
+#include <Servo.h>
 
-/*
-  Turns on an LED on for one second, then off for one second, repeatedly.
-*/
+#define SERVOPIN 5
+#define CAPTEURINTERRUPNUMBER 0
+
+Servo serv;
+
+int compteur(0);
+
+void updatePulseCompteur(){
+    compteur++;
+}
 
 void setup()
 {
 	Serial.begin(9600);
 	Serial.println(F("Test fourche optique"));
-	// initialize the digital pin as an output.
-	// Pin 13 has an LED connected on most Arduino boards:
-	pinMode(13, OUTPUT);
+	serv.attach(SERVOPIN);
+    attachInterrupt(CAPTEURINTERRUPNUMBER, updatePulseCompteur, CHANGE );
 }
 
 void loop()
 {
-
+  for (int pos = 10; pos <= 170; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    serv.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(10);                       // waits 15ms for the servo to reach the position
+  }
+  for (int pos = 170; pos >= 10; pos -= 1) { // goes from 180 degrees to 0 degrees
+    serv.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(10);                       // waits 15ms for the servo to reach the position
+  }
 
 }
